04edda3748ca7bdb50a4bbd92a311897b1c626f0
VitorM34 <vitorluistresoldiluis@outlook.com>
2026-09-14 15:16:34 -0300

    fix: avoid sync-over-async on startup and register global exception handler

--- src/NotifyFlow.Api/Program.cs
+++ src/NotifyFlow.Api/Program.cs
@@ -1,15 +1,25 @@
 using NotifyFlow.Api.Endpoints;
+using NotifyFlow.Api.ErrorHandling;
 using NotifyFlow.Api.Messaging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<IRabbitMqPublisher>(sp =>
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+using var bootstrapLoggerFactory = LoggerFactory.Create(logging =>
 {
-    var config = sp.GetRequiredService<IConfiguration>();
-    return RabbitMqPublisher.CreateAsync(config).GetAwaiter().GetResult();
+    logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+    logging.AddConsole();
 });
+var publisherLogger = bootstrapLoggerFactory.CreateLogger<RabbitMqPublisher>();
+
+var rabbitMqPublisher = await RabbitMqPublisher.CreateAsync(builder.Configuration, publisherLogger);
+builder.Services.AddSingleton<IRabbitMqPublisher>(rabbitMqPublisher);
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 app.MapEventEndpoints();
 
